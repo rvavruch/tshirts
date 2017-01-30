454d4904e652abba93b0f1b0cd6061ad0cee7205
Rudolf Vavruch <rudolf@antler.co.za>
2017-01-30 21:36:10 +0200

    Refactored based on suggestions

--- tshirt.go
+++ tshirt.go
@@ -1,77 +1,123 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "time"
+	"flag"
+	"fmt"
+	"time"
 )
 
-func even(number int) bool {
-    return number%2 == 0
+type ClothesState struct {
+	day     time.Time
+	clean   int
+	dirty   int
+	washing int
+	drying  int
 }
 
-func isWashingDay(today time.Time, thursday bool) bool {
-    if today.Format("Mon") == "Mon" {
-	    fmt.Println("Monday!")
-	    return true
-    } else if thursday && today.Format("Mon") == "Thu" {
-	    _, week := today.ISOWeek()
-	    if even(week) {
-		    fmt.Println("Thursday!")
-		    return true
-	    }
-    }
-    return false
+func (cs ClothesState) String() string {
+	return format(cs.day.Format("2006-01-02"), cs.clean, cs.dirty, cs.washing, cs.drying)
+}
+
+func (cs ClothesState) Advance(doDrying, doThursdays bool) ClothesState {
+	tomorrow := cs.day.AddDate(0, 0, 1)
+
+	clean := cs.clean
+	clean += cs.drying
+
+	drying := cs.washing
+	if !doDrying {
+		// take all the drying directly in to clean
+		clean += drying
+		drying = 0
+	}
+
+	washing := 0
+	dirty := cs.dirty
+	if isWashingDay(tomorrow, doThursdays) {
+		washing = dirty
+		dirty = 0
+	}
+
+	clean--
+	dirty++
+
+	return ClothesState{
+		day:     tomorrow,
+		clean:   clean,
+		dirty:   dirty,
+		washing: washing,
+		drying:  drying,
+	}
+
+}
+
+func even(number int) bool {
+	return number%2 == 0
+}
+
+func format(day, clean, dirty, washing, drying interface{}) string {
+	return fmt.Sprintf("%-11v| %-2v| %-2v| %-2v| %-2v", day, clean, dirty, washing, drying)
+}
+
+func isWashingDay(today time.Time, doThursday bool) bool {
+
+	if today.Weekday() == time.Monday {
+		fmt.Println(today.Weekday().String())
+		return true
+	}
+	
+	if doThursday && today.Weekday() == time.Thursday {
+		_, week := today.ISOWeek()
+		if even(week) {
+			fmt.Println(today.Weekday().String())
+			return true
+		}
+	}
+	
+	return false
 }
 
 func main() {
-    numberOftshirtsPtr := flag.Int("shirts", 9, "a int number of tshirts")
-    thursdayPtr := flag.Bool("thursday", true, "a bool use Thursday")
-    dryingPtr := flag.Bool("drying", true, "a bool use drying")
-    flag.Parse()
+	numberOftshirts := 9
+	doThursdays := true
+	doDrying := false
+	
+	flag.IntVar(&numberOftshirts, "shirts", numberOftshirts, "the number of tshirts")
+	flag.BoolVar(&doThursdays, "thursday", doThursdays, "use Thursday")
+	flag.BoolVar(&doDrying, "drying", doDrying, "use drying")
+	flag.Parse()
 
-    var clean, minClean = *numberOftshirtsPtr, *numberOftshirtsPtr
-    var dirty, washing, drying int
-    today, _ := time.Parse(time.RFC3339, "2017-02-06T00:00:00+00:00")
+	today, _ := time.Parse(time.RFC3339, "2017-02-06T00:00:00+00:00")
+	
+	// Set the state with 1 dirty shirt to match OP logic
+	state := ClothesState{
+		day:   today,
+		clean: numberOftshirts - 1,
+		dirty: 1,
+	}
+	
+	minClean := state.clean
+	
+	fmt.Println(format("Date", "C", "D", "W", "Y"))
+	fmt.Printf("%v\n", state)
+	
+	testDays := 365
+	i := 0
+	for ; i < testDays; i++ {
+		state = state.Advance(doDrying, doThursdays)
+		fmt.Printf("%v\n", state)
 
-    fmt.Println("Date       | C | D | W | Y ")
-
-    testDays := 365
-    for i := 0; i < testDays; i++ {
-	    if *dryingPtr {
-		    clean += drying
-		    drying = 0
-
-		    drying = washing
-		    washing = 0
-	    } else {
-		    clean += washing
-		    washing = 0
-	    }
-
-	    if clean == 0 {
-		    fmt.Println("Run out of clean shirts!")
-		    break
-	    }
-
-	    if isWashingDay(today, *thursdayPtr) {
-		    washing = dirty
-		    dirty = 0
-	    }
-
-	    // take a clean tshirt and wear it - it immediately becomes dirty
-	    clean--
-	    dirty++
-
-	    if clean < minClean {
-		    minClean = clean
-	    }
-
-	    fmt.Printf("%s | %d | %d | %d | %d \n", today.Format("2006-01-02"), clean, dirty, washing, drying)
-
-	    today = today.AddDate(0, 0, 1)
-    }
-
-    fmt.Println("Total days:", testDays)
-    fmt.Println("Minimum clean shirts:", minClean)
+		if state.clean < minClean {
+			minClean = state.clean
+		}
+		
+		if state.clean < 0 {
+			fmt.Println("Run out of clean shirts!")
+	            	break
+		}
+	}
+	
+	fmt.Println("Total days:", i)
+	fmt.Println("Minimum clean shirts:", minClean)
 }
+
